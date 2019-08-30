12dca3766e658acd5057bc115264358e5c3a6168
Julia Sizon <j.sizon@aimprosoft.com>
2019-08-30 17:28:07 +0300

    implement tutorial1 demo consumer

--- build.sbt
+++ build.sbt
@@ -5,8 +5,9 @@ version := "0.1"
 scalaVersion := "2.13.0"
 
 val kafkaVersion = "2.3.0"
+val slf4jVersion = "1.7.28"
 
 libraryDependencies ++= Seq(
   "org.apache.kafka" % "kafka-clients" % kafkaVersion,
-  "org.slf4j" % "slf4j-simple" % "1.7.28"
+  "org.slf4j" % "slf4j-simple" % slf4jVersion
 )
---
+++ src/main/scala/com/tutorial1/ConsumerDemo.scala
@@ -0,0 +1,42 @@
+package com.tutorial1
+
+import java.time.Duration
+import java.util
+import java.util.Properties
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecords, KafkaConsumer}
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.jdk.CollectionConverters._
+
+object ConsumerDemo extends App {
+
+  val logger: Logger = LoggerFactory.getLogger("ConsumerDemo")
+
+  val server = "localhost:9092"
+  val groupId = "my-fourth-application"
+  val topic = "first_topic"
+
+  val props = new Properties()
+
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server)
+  props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
+  props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+  val consumer = new KafkaConsumer[String, String](props)
+
+  consumer.subscribe(util.Arrays.asList(topic))
+
+  while (true) {
+    val records: ConsumerRecords[String, String] = consumer.poll(Duration.ofMillis(100))
+
+    records. /*iterator().*/ asScala.foreach(r => {
+      logger.info("Key: " + r.key + ", Value: " + r.value)
+      logger.info("Partion: " + r.partition + ", Offset: " + r.offset)
+    })
+
+  }
+
+}
